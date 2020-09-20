e494cf6d5c3261081a7f2513201ce3beac414c35
iKnock <ergana.henok@gmail.com>
2020-09-20 19:05:10 +0200

    add backend configs

--- src/App.js
+++ src/App.js
@@ -1,8 +1,23 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import { withAuthenticator, AmplifySignOut } from '@aws-amplify/ui-react'
 
 function App() {
+  return (
+    <div className="App">
+      <header>
+        <img src={logo} className="App-logo" alt="logo" />
+        <h1>We now have Auth!</h1>
+      </header>
+      <AmplifySignOut />
+    </div>
+  );
+}
+
+export default withAuthenticator(App);
+
+/**function App() {
   return (
     <div className="App">
       <header className="App-header">
@@ -23,4 +38,4 @@ function App() {
   );
 }
 
-export default App;
+export default App;**/
